5ebfd56fa5357a361f17b527455195e8143a2789
Kirstin Barnett <123597290+kknb1982@users.noreply.github.com>
2024-12-04 14:09:13 +0000

    made code more efficient

--- weather.sh
+++ weather.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-wget -P data/weather/`date +%Y%m%d_%H%M%S.json` https://prodapi.metweb.ie/observations/athenry/today
+wget -q -O data/weather/`date +"%Y%m%d_%H%M_athenry.json"` "https://prodapi.metweb.ie/observations/athenry/today"
+echo "Weather data downloaded"
